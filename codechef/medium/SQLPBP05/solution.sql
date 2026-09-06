51f63c8c28ca937675b2ac1d84f226e999edf4fe
Nidhi Kumari Prasad <nidprasad03@gmail.com>
2026-09-06 23:43:48 +0530

    ✅ SQLPBP05 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/SQLPBP05/solution.sql
@@ -0,0 +1,7 @@
+-- your code goes here
+SELECT m.match_id, m.player_1, m.player_2, m.winner, m.match_date, p.score
+FROM Players p 
+JOIN Matches m 
+ON p.player_name=m.winner
+ORDER BY m.match_date DESC 
+LIMIT 5;
